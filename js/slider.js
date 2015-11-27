8173b489446158114f10ce82b20a578af1195f67
ilyashmaxim <maxim.ilyash.dps@gmail.com>
2015-11-27 10:20:34 +0200

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -9,24 +9,11 @@
 		this.slides = data.slice();
 		this.Builder();
 		this.$selfy = $('.gallery>ul');
-		//var sizeI;
-		//$('.classImg').eq(0).load(function() {
-		//	sizeI = $(this).width();
-		//});
-		//$("<img/>") // Сделаем в памяти копию этой картинки, чтобы избежать проблем с CSS
-		//	.attr("src", this.slides[0])
-		//	.load(function() {
-		//		// Здесь копия нашей картинки загружена и можно получить её размеры
-		//		sizeI = this.width; // Учтите: $(this).width() не сработает
-		//	});
-		//this.sizeImg = sizeI;
-
 		this.sizeImg = $('.classImg').eq(0).width();
 		this.index = 0;
 		this.marginStart = -this.sizeImg;
 		this.AnimateImg();
 		this.SelectImg();
-
 	}
 	Slider.prototype.Builder = function() {
 		var conteiner = document.createElement('div');
@@ -71,17 +58,14 @@
 				.css({
 					'margin-left': _self.marginStart
 				});
-			/*.stop();*/
 			clearInterval(_self.interval);
 			_self.marginStart = 0;
 			setTimeout(_self.AnimateImg(), 3000);
-			//_self.AnimateImg();
 		});
 	};
 	Slider.prototype.AnimateImg = function() {
 		this.index = 0;
 		var duration = 800;
-		// var $selfy = $('.gallery>ul');
 		// var sizeImg = $('.classImg').width();
 		this.interval = setInterval(Anim.bind(this), 2000);
 
@@ -91,11 +75,10 @@
 			this.$selfy.animate({
 				'margin-left': _self.marginStart
 			}, duration, function() {
-				//_self.HoverByIndex();
+				_self.HoverByIndex();
 				_self.marginStart -= _self.sizeImg;
 				if (_self.marginStart === -(_self.sizeImg * _self.slides.length)) {
 					_self.marginStart = 0;
-					_self.index = 0;
 					duration = 100;
 				} else {
 					duration = 800;
@@ -106,8 +89,9 @@
 
 	Slider.prototype.HoverByIndex = function() {
 		var _self = this;
+		if (this.index === this.slides.length) this.index = 0;
 		var nextIndex = this.index + 1;
-		if (this.index === this.slides.length - 1) {
+		if (nextIndex === this.slides.length) {
 			nextIndex = 0;
 		}
 		$('.navi').find('[value=' + _self.index + ']').removeClass('active');

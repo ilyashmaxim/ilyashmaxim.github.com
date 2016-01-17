0dc4291a2031f2d702f2a66c09e6f960badd4566
ilyashmaxim <maxim.ilyash.dps@gmail.com>
2016-01-17 16:28:14 +0200

    fix test

--- test/js/script.js
+++ test/js/script.js
@@ -68,7 +68,7 @@
 
 		var productView = document.createElement('div');
 		$(productView).addClass('productView').each(function() {
-			$.getJSON('./src/info_box.json', {}, function(json) {
+			$.getJSON('./src/info_box.json', function(json) {
 				var $container = $('container').find('productView');
 				for (var i = 0; i < json.length; i += 1) {
 					var product = document.createElement('div');
